14be4e9d19597199a6b2d9bcc94ffdaccc08ebd5
Avi Asthana <aviasthana@gmail.com>
2026-02-12 15:11:23 +0000

    Add supply event corpus

---
+++ src/commodity_supply_shock_detector/data/events.py
@@ -0,0 +1,6 @@
+SUPPLY_EVENTS = [
+    {"date": "2026-01-10", "commodity": "CL", "headline": "Pipeline outage cuts Gulf Coast crude flows"},
+    {"date": "2026-01-15", "commodity": "NG", "headline": "Freezing weather lifts nat gas demand surge"},
+    {"date": "2026-01-22", "commodity": "HG", "headline": "Chile port strike disrupts copper exports"},
+    {"date": "2026-02-01", "commodity": "ZW", "headline": "Drought conditions worsen Black Sea grain outlook"},
+]
